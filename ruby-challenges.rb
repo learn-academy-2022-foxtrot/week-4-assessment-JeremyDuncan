6e8e28182e3965e3c624d56392e8fd8c60c7f5ba
Jeremy Duncan <jduncan46@my.devry.edu>
2022-09-30 16:45:38 -0400

    updated formatting to 80 char std

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -1,7 +1,10 @@
-# ASSESSMENT 4: Ruby Coding Practical Questions
-# MINASWAN ✌️
+# ASSESSMENT 4: Ruby Coding Practical Questions --------------------------------
+# MINASWAN ✌️ ------------------------------------------------------------------
 
-# --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+#*******************************************************************************
+# --------------------1)👉 Create a method that takes in a number and determines 
+# if the number is even or odd. Use the test variables provided.
+#*******************************************************************************
 
 num1 = 7
 # Expected output: '7 is odd'
@@ -12,8 +15,12 @@ num3 = 221
 
 
 
-# -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
-# HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+#*******************************************************************************
+# -------------------2)👉 Create a method that takes in a string and removes all 
+# the vowels from the string. Use the test variables provided. 
+# HINT: Check out this resource:
+# https://ruby-doc.org/core-2.6/String.html#method-i-delete
+#*******************************************************************************
 
 beatles_album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -24,7 +31,11 @@ beatles_album3 = 'Abbey Road'
 
 
 
-# -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+#*******************************************************************************
+# -------------------3)👉 Create a method that takes in a string and checks if 
+# the string is a palindrome. A palindrome is the same word spelled forward or 
+# backward. Use the test variables provided.
+#*******************************************************************************
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
